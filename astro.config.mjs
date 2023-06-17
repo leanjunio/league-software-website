151ce4fa7989285b66f95d133b2495a70bdb2e72
Lean Junio <leanjunio@outlook.com>
2023-06-17 16:28:11 -0400

    deploy to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
+  output: "static",
   adapter: vercel({
     analytics: true
   })
